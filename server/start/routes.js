a17a58420267961c02f8cf054e786b50608e1ce4
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-10-09 17:21:40 +0500

    updated

--- server/start/routes.js
+++ server/start/routes.js
@@ -1,4 +1,10 @@
+const helmet = require("helmet");
+const compression = require("compression");
+const portfolio = require("../routes/portfolio");
 const error = require("../middleware/error");
 module.exports = (app) => {
+  app.use(helmet());
+  app.use(compression());
+  app.use("/api/get", portfolio);
   app.use(error);
 };
