3816a4e0758ef1226caba806a07b0d055af40111
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-11-22 14:38:57 +0500

    .

--- client/pages/api/auth/[...nextauth].ts
+++ client/pages/api/auth/[...nextauth].ts
@@ -4,14 +4,12 @@ import FacebookProvider from "next-auth/providers/facebook";
 export default NextAuth({
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID || "6cf7a148e6cdef7f3564",
-      clientSecret:
-        process.env.GITHUB_SECRET || "5d71b2d7fdeb73c3c0395c0bed110344d8ef8647",
+      clientId: process.env.GITHUB_ID || '6cf7a148e6cdef7f3564',
+      clientSecret: process.env.GITHUB_SECRET || "5d71b2d7fdeb73c3c0395c0bed110344d8ef8647",
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID || "454830186669253",
-      clientSecret:
-        process.env.FACEBOOK_SECRET || "1b7d8f6071b10ed8a9248714cbe93bbe",
+      clientSecret: process.env.FACEBOOK_SECRET || "1b7d8f6071b10ed8a9248714cbe93bbe",
     }),
   ],
 });
