208e655db5c4dfbb8744cccb7cc3a0229b34953f
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-12-04 09:56:47 +0000

    some updated && readstream working, but not normal working

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -12,7 +12,11 @@ import { useState } from "react";
 import Footer from "components/Footer";
 import { GetServerSideProps } from "next";
 import { portfolio } from "types/portfolio";
-const Index = ({data}: {data: portfolio[]}) => {
+const Index = ({ data }: { data: portfolio[] }) => {
+  var url: string = process.env.SERVER_URL || "";
+  if (process.env.NODE_ENV === "development") {
+    url = "http://localhost:4000";
+  }
   const [text, setText] = useState("");
   return (
     <div>
@@ -27,111 +31,36 @@ const Index = ({data}: {data: portfolio[]}) => {
           handleChange={(e) => setText(e.target.value)}
           value={text}
         />
-        <div className={s.post}>
-          <Image
-            className={s.postImage}
-            loading="lazy"
-            placeholder="blur"
-            loader={() => "https://wallpaperaccess.com/full/138728.jpg"}
-            blurDataURL="https://cdn.pixabay.com/photo/2015/06/24/02/12/the-blurred-819388_1280.jpg"
-            height={450}
-            width={800}
-            src="https://wallpaperaccess.com/full/138728.jpg"
-          />
-          <div className={s.desc}>
-            <div className={s.profile}>
-              <img
-                className={s.profileImage}
-                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-              />
-              <p>Sanjarbek Ismatov</p>
-            </div>
-            <h1>MyBlog</h1>
-            <div>
-              <FontAwesomeIcon className={s.icon} icon={faHeart} />
-              <p>10</p>
-            </div>
-          </div>
-        </div>
-        
-        <div className={s.post}>
-          <Image
-            className={s.postImage}
-            loading="lazy"
-            placeholder="blur"
-            loader={() => "https://wallpaperaccess.com/full/138728.jpg"}
-            blurDataURL="https://cdn.pixabay.com/photo/2015/06/24/02/12/the-blurred-819388_1280.jpg"
-            height={450}
-            width={800}
-            src="https://wallpaperaccess.com/full/138728.jpg"
-          />
-          <div className={s.desc}>
-            <div className={s.profile}>
-              <img
-                className={s.profileImage}
-                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-              />
-              <p>Sanjarbek Ismatov</p>
-            </div>
-            <h1>MyBlog</h1>
-            <div>
-              <FontAwesomeIcon className={s.icon} icon={faHeart} />
-              <p>10</p>
-            </div>
-          </div>
-        </div>
-        <div className={s.post}>
-          <Image
-            className={s.postImage}
-            loading="lazy"
-            placeholder="blur"
-            loader={() => "https://wallpaperaccess.com/full/138728.jpg"}
-            blurDataURL="https://cdn.pixabay.com/photo/2015/06/24/02/12/the-blurred-819388_1280.jpg"
-            height={450}
-            width={800}
-            src="https://wallpaperaccess.com/full/138728.jpg"
-          />
-          <div className={s.desc}>
-            <div className={s.profile}>
-              <img
-                className={s.profileImage}
-                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-              />
-              <p>Sanjarbek Ismatov</p>
-            </div>
-            <h1>MyBlog</h1>
-            <div>
-              <FontAwesomeIcon className={s.icon} icon={faHeart} />
-              <p>10</p>
-            </div>
-          </div>
-        </div>
-        <div className={s.post}>
-          <Image
-            className={s.postImage}
-            loading="lazy"
-            placeholder="blur"
-            loader={() => "https://wallpaperaccess.com/full/138728.jpg"}
-            blurDataURL="https://cdn.pixabay.com/photo/2015/06/24/02/12/the-blurred-819388_1280.jpg"
-            height={450}
-            width={800}
-            src="https://wallpaperaccess.com/full/138728.jpg"
-          />
-          <div className={s.desc}>
-            <div className={s.profile}>
-              <img
-                className={s.profileImage}
-                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-              />
-              <p>Sanjarbek Ismatov</p>
-            </div>
-            <h1>MyBlog</h1>
-            <div>
-              <FontAwesomeIcon className={s.icon} icon={faHeart} />
-              <p>10</p>
-            </div>
-          </div>
-        </div>
+        {data.map((e, i) => {
+          return (
+            <><div className={s.post}>
+              <Image
+                className={s.postImage}
+                loading="lazy"
+                placeholder="blur"
+                loader={() => `${url}/image/${e.images[0]}`}
+                blurDataURL="https://cdn.pixabay.com/photo/2015/06/24/02/12/the-blurred-819388_1280.jpg"
+                height={450}
+                width={800}
+                src={`${url}/image/${e.images[0]}`} />
+              <div className={s.desc}>
+                <div className={s.profile}>
+                  <img
+                    className={s.profileImage}
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
+                  <p>Sanjarbek Ismatov</p>
+                </div>
+                <h1>MyBlog</h1>
+                <div>
+                  <FontAwesomeIcon className={s.icon} icon={faHeart} />
+                  <p>10</p>
+                </div>
+              </div>
+            </div></>
+          )
+        })}
+
+
       </main>
       {/* <script
         type="text/javascript"
@@ -153,9 +82,15 @@ const Index = ({data}: {data: portfolio[]}) => {
     </div>
   );
 };
-export const getServerSideProps: GetServerSideProps<{data: portfolio[]}> = async ({params}) => {
-  const res = await fetch(`${process.env.SERVER_URL}/api/portfolio/all`)
+export const getServerSideProps: GetServerSideProps<{ data: portfolio[] }> = async ({ params }) => {
+  var url: string = process.env.SERVER_URL || "";
+  if (process.env.NODE_ENV === "development") {
+    url = "http://localhost:4000";
+  }
+  const res = await fetch(`${url}/api/portfolio/all`)
+
   const data = await res.json()
+  console.log(data)
   return {
     props: {
       data: data
