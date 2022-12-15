03121b0f51db09c00e5ff19ce6e7b05c2784d891
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2022-12-15 21:05:39 +0500

    new features adding

---
+++ client/utils/getDetails.ts
@@ -0,0 +1,15 @@
+import { portfolio } from "types/portfolio";
+
+export async function getLikeFromPortfolio(
+  data: portfolio[],
+
+  userId: string
+) {
+  //   const totalCount = likedArray.length;
+  //   const isLiked = userId in likedArray;
+  //   return {
+  //     count: totalCount,
+  //     isLiked: isLiked,
+  //   };
+}
+export async function getMe() {}
