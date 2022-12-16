94c41197c3caecdb2103ac28f5f0364df2b2fac4
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2022-12-16 09:06:22 +0500

    url updated

--- client/utils/serverUrl.ts
+++ client/utils/serverUrl.ts
@@ -2,6 +2,8 @@ export function serverUrl() {
   var url: string = process.env.SERVER_URL || "";
   if (process.env.NODE_ENV === "development") {
     url = "http://localhost:4000";
+  } else {
+    url = "https://portfolio-master.onrender.com";
   }
   return url;
 }
