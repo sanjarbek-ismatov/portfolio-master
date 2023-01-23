280537eebb38d8e737cfc58813be27072e54b6c7
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-01-23 16:43:45 +0500

    filter updated

--- client/utils/filterByUsed.ts
+++ client/utils/filterByUsed.ts
@@ -5,23 +5,27 @@ export const filterByUsed = (data: portfolio[]) => {
   return new Promise((resolve, reject) => {
     const all: string[] = [];
     data.map((e) => all.push(...e.used));
-
     resolve(Array.from(new Set(all)));
   }) as Promise<string[]>;
 };
 export const filterToUrl = (router: NextRouter, newUsed: string) => {
-  if (typeof router.query.filter === "string") {
-    const array = router.query.filter.split(",");
+  if (router.query.filter) {
+    const array = router.query.filter.toString().split(",");
     if (array.includes(newUsed)) {
       array.splice(array.indexOf(newUsed), 1);
     } else {
       array.push(newUsed);
     }
     const arrayToUrl = array.join(",");
-    router.push({
-      pathname: "/page/1",
-      query: { filter: arrayToUrl },
-    });
+    if (!arrayToUrl) {
+      router.push("/page/1");
+    } else {
+      router.push({
+        pathname: "/page/1",
+        query: { filter: arrayToUrl },
+      });
+    }
   }
+
   return true;
 };
