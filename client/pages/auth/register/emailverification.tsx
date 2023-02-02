791976d38c8391ec2b276dcfc01f6d5ddbd6fd45
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-02-02 12:24:53 +0000

    updated

--- client/pages/auth/register/emailverification.tsx
+++ client/pages/auth/register/emailverification.tsx
@@ -1,29 +1,37 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
 import s from "styles/L.module.scss";
 import { useForm } from "react-hook-form";
 import { useVerifyEmailMutation } from "state/api/portfolioApi";
-import Dialog from 'components/Dialog';
+import Dialog from "components/Dialog";
 const EmailVerification = () => {
-  const [sendMail, { data, isLoading }] = useVerifyEmailMutation();
+  const [sendMail, { data, isLoading, error }] = useVerifyEmailMutation();
+  function handleSubmitEmail(formData: FormData){
+    setMessage('Yuborilmoqda...')
+    sendMail(formData)
+  }
+  const [message, setMessage] = useState("");
   const { handleSubmit, register } = useForm<FormEvent>();
   return (
-    <div className={s.container}>
-      <div className={s.form}>
-        <form
-          onSubmit={handleSubmit((formData) => sendMail(formData))}
-          className={s.formik}
-        >
-          <input
-            className={s.input}
-            type="email"
-            {...register("email")}
-            placeholder="Pochta"
-            required
-          />
-          <button className={s.button}>Tasdiqlash</button>
-        </form>
+    <>
+      <div className={s.container}>
+        <div className={s.form}>
+          <form
+            onSubmit={handleSubmit((formData) => handleSubmitEmail(formData))}
+            className={s.formik}
+          >
+            <input
+              className={s.input}
+              type="email"
+              {...register("email")}
+              placeholder="Pochta"
+              required
+            />
+            <button className={s.button}>Tasdiqlash</button>
+          </form>
+        </div>
       </div>
-    </div>
+      {message && <Dialog  />}
+    </>
   );
 };
 
