5edeea7f1754ca85719ab544d77a222a87329cf2
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-02-07 19:47:50 +0500

    back navigate enabled

--- client/components/Navbar/NavbarProfile.tsx
+++ client/components/Navbar/NavbarProfile.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import styles from "./NavbarProfile.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/router";
 const NavbarProfile = () => {
+  const router = useRouter();
   return (
     <nav className={styles.nav}>
-      <FontAwesomeIcon className={styles.backIcon} icon={faArrowLeft} />
+      <FontAwesomeIcon
+        onClick={() => router.back()}
+        className={styles.backIcon}
+        cursor="pointer"
+        icon={faArrowLeft}
+      />
     </nav>
   );
 };
